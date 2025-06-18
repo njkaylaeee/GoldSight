0eb412a2e9bbb78fda02b4d4daaf77baa817d58e
njkaylaeee <najwazulfa655@students.unnes.ac.id>
2025-06-18 10:27:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.set_page_config(page_title="GoldSight", layout="wide")
+
+st.title("💰 GoldSight - Prediksi Harga Emas")
+st.markdown("""
+Selamat datang di GoldSight!\n
+Dashboard ini memiliki tiga bagian utama:
+- 📊 Dataset & Visualisasi
+- 🧠 Model yang Digunakan
+- 📈 Formulir Prediksi Harga Emas
+
+Gunakan sidebar di kiri untuk menjelajahi setiap bagian.
+""")
