00acb24b998e4725fdb6133b13cb8882984159a7
njkaylaeee <najwazulfa655@students.unnes.ac.id>
2025-06-18 12:04:00 +0700

    Update 1_Data_Exploration.py

--- pages/1_Data_Exploration.py
+++ pages/1_Data_Exploration.py
@@ -1,24 +1,11 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import matplotlib.pyplot as plt
-import os
 
-st.title("📊 Dataset & Visualisasi")
+st.title("Dataset & Visualisasi")
 
-# Pastikan path-nya sesuai
-csv_path = os.path.join(os.path.dirname(__file__), "..", "clean_gold_data.csv")
-df = pd.read_csv(csv_path)
+# Pastikan path-nya relatif ke root folder
+df = pd.read_csv("final_gold_data.csv")
 
 st.subheader("Cuplikan Data")
 st.dataframe(df.head())
-
-st.subheader("Statistik Deskriptif")
-st.write(df.describe())
-
-st.subheader("Visualisasi Harga Emas")
-fig, ax = plt.subplots(figsize=(10, 4))
-ax.plot(df['close'], color='gold', label='Harga Penutupan')
-ax.set_title("Tren Harga Emas (Close)")
-ax.set_ylabel("Harga")
-ax.grid(True)
-st.pyplot(fig)
